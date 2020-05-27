5f4ef0c8845d6bc284b420398e934dd1cc1c6620
Masamichi Takagi <masamichi.takagi@riken.jp>
2020-05-27 14:39:14 +0900

    ltp: tools: refactor

---
+++ tools/gen_autotest_scripts.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# Usage: cd <ltp-install>/bin && cat ../runtest/{syscalls,ipc,mm,hugetlb} | ./gen_autotest_scripts.awk
+# This puts scritps on <autotest>/data/scripts/
+#
+# Usage of the generated scripts:
+# McKernel run: bash -x <autotest>/data/script/ltp-<test_name>
+
+SCRIPT_PATH=$(readlink -m "${BASH_SOURCE[0]}")
+AUTOTEST_HOME="${SCRIPT_PATH%/*/*/*/*}"
+scriptdir="$AUTOTEST_HOME/data/scripts"
+rm -f $scriptdir/ltp-*
+
+while read testcase command_line; do
+    [[ $testcase =~ ^# ]] && continue
+    script=$scriptdir/ltp-$testcase
+
+cat > $script <<'EOF'
+#!/bin/sh
+
+set -x
+
+SCRIPT_PATH=$(readlink -m "${BASH_SOURCE[0]}")
+AUTOTEST_HOME="${SCRIPT_PATH%/*/*/*}"
+. $AUTOTEST_HOME/bin/config.sh
+
+EOF
+
+cat >> $script <<EOF
+testcase=$testcase
+command_line=$command_line
+
+EOF
+
+cat >> $script <<'EOF'
+recorddir=$WORKDIR/output/ltp-$testcase
+command_line="$testcase $command_line"
+
+EOF
+
+cat >> $script <<'EOF'
+. $AUTOTEST_HOME/ltp/install/bin/run.sh
+EOF
+
+chmod +x $script
+done
--- tools/run.sh
+++ tools/run.sh
@@ -1,52 +1,27 @@
 #!/bin/sh
 
-SCRIPT_PATH=$(readlink -m "${BASH_SOURCE[0]}")
-AUTOTEST_HOME="${SCRIPT_PATH%/*/*/*/*}"
-export LTPROOT=$AUTOTEST_HOME/ltp/install
-
-ihkosctl=$MCKINSTALL/sbin/ihkosctl
-
 # Prepare working directory
 rm -rf $recorddir && mkdir -p $recorddir
 
-$ihkosctl 0 clear_kmsg
+$MCKINSTALL/sbin/ihkosctl 0 clear_kmsg
 
 # Run LTP
 pushd $recorddir > /dev/null
 echo "$command_line" > $recorddir/command_line
-sudo bash -c " LTPMCEXEC=$MCKINSTALL/bin/mcexec MCEXEC_TIMEOUT=10800 MC_RESET_EACHTIME=0 $LTPROOT/runltp -l LTP_RUN.log -f $recorddir/command_line"
+sudo bash -c " LTPMCEXEC=$MCKINSTALL/bin/mcexec MCEXEC_TIMEOUT=10800 MC_RESET_EACHTIME=0 $AUTOTEST_HOME/ltp/install/runltp -l LTP_RUN.log -f $recorddir/command_line"
 exit_code=$?
 popd > /dev/null
 
 # OK/NG decision
 rc=0
-if [[ $exit_code != 0 ]]; then
-    rc=1
-fi
+[[ $exit_code != 0 ]] && rc=1
 
 # Check if kmsg is empty
 $MCKINSTALL/sbin/ihkosctl 0 kmsg > $recorddir/kmsg.log
-if [ "`cat $recorddir/kmsg.log | wc -l`" -ne 1 ]; then
-    echo "$(basename $0): WARNING: kmsg isn't empty."
-fi
+[[ "$(cat $recorddir/kmsg.log | wc -l)" != 1 ]] && echo "$(basename $0): WARNING: kmsg isn't empty."
 
 # Check if process/thread structs remain
-show_struct_process_or_thread="$recorddir/show_struct_process_or_thread.log"
-$ihkosctl 0 clear_kmsg
-$ihkosctl 0 ioctl 40000000 1
-$ihkosctl 0 ioctl 40000000 2
-$ihkosctl 0 kmsg > $show_struct_process_or_thread
-
-nprocs=`awk '$4=="processes"{print $3}' $show_struct_process_or_thread`
-if [ -n $nprocs ] && [ "$nprocs" != "0" ]; then
-    echo "$(basename $0): INFO: $nprocs process(es) remaining"
-    rc=1
-fi
-
-nthreads=`awk '$4=="threads"{print $3}' $show_struct_process_or_thread`
-if [ -n $nthreads ] && [ "$nthreads" != "0" ]; then
-    echo "$(basename $0): INFO: $nprocs thread(s) remaining"
-    rc=1
-fi
+! $AUTOTEST_HOME/bin/getnumprocess.sh && echo "$(basename $0): INFO: $nprocs process(es) remaining" && rc=1
+! $AUTOTEST_HOME/bin/getnumthread.sh && echo "$(basename $0): INFO: $nprocs thread(s) remaining" && rc=1
 
 exit $rc
