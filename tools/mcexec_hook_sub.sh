d0fc5722e6f99b5634bedac8b61b11f12482213e
Masamichi Takagi <masamichi.takagi@riken.jp>
2020-07-03 12:14:45 +0900

    fix timeout

--- tools/mcexec_hook_sub.sh
+++ tools/mcexec_hook_sub.sh
@@ -3,11 +3,7 @@
 sync; sync; sync;
 
 if [ $# -eq 1 ]; then
-    file $1 | egrep -i "(script|text)" 2>&1 >/dev/null;
-    if [ $? -ne 0 ]; then
-	eval ${LTPMCEXEC} $*;
-	exit $?;
-    fi;
-fi;
-eval $*;
-
+    eval ${LTPMCEXEC} $*;
+else
+    eval $*;
+fi
